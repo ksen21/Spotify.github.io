afdb9136a687716bf7fea35c955d4ba13f250d6c
kartik sen <karthiksen10@gmail.com>
2022-05-17 15:40:25 +0530

    second try

---
+++ script.js
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+let songIndex=0;
+let audioElement = new Audio('1.mp3');
+let masterPlay=document.getElementById('masterPlay');
+let myprograssBar=document.getElementById('PrograssBar');
+let gif=document.querySelector('.gif');
+let songItem=Array(document.getElementsByClassName('songItem'));
+
+
+let songs=[
+    {songName: "Let me love you",songPath:'songs/1.mp3',coverPath:'covers/1.jpg'},
+    {songName: "Let me love 2",songPath:'songs/2.mp3',coverPath:'covers/2.jpg'},
+    {songName: "Let me love 3",songPath:'songs/3.mp3',coverPath:'covers/3.jpg'},
+    {songName: "Let me love 4",songPath:'songs/4.mp3',coverPath:'covers/4.jpg'},
+    {songName: "Let me love 5",songPath:'songs/5.mp3',coverPath:'covers/5.jpg'},
+    {songName: "Let me love 6",songPath:'songs/6.mp3',coverPath:'covers/6.jpg'},
+]
+
+
+// Handle play and pause
+
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime===0){
+        audioElement.play();
+        masterPlay.classList.remove("fa-circle-play");
+        masterPlay.classList.add("fa-circle-pause");
+        gif.classList.remove('opacity-0')
+
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove("fa-circle-pause");
+        masterPlay.classList.add("fa-circle-play");
+        gif.classList.add('opacity-0')
+
+    }
+})
+
+// listin to events
+
+audioElement.addEventListener('timeupdate',()=>{
+    console.log('timeupdate');
+    // update seek bar
+    let prograss=parseInt(audioElement.currentTime/audioElement.duration*100);
+    myprograssBar.value=prograss;
+})
+
+myprograssBar.addEventListener('change',()=>{
+    audioElement.currentTime=myprograssBar.value*audioElement.duration/100;
+})
+
+songItem.forEach((element,i)=>{
+    console.log(element,i);
+    // element.getElementByTagName("img")[0].src=songs[i].coverPath;
+    // element.getElementsByClassName('songName')[0].innerText=songs[i].songName;
+
+})
