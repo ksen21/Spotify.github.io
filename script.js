e893350139f0a4d121ee6aa073de130ea0b1e68e
kartik sen <karthiksen10@gmail.com>
2022-05-18 21:35:37 +0530

    Song Update

--- script.js
+++ script.js
@@ -6,6 +6,8 @@
 
 let songIndex=0;
 let audioElement = new Audio('1.mp3');
+let jahan = new Audio('Jahaan Tum Ho.mp3');
+let btn_1=document.querySelector('.btn-1');
 let masterPlay=document.getElementById('masterPlay');
 let myprograssBar=document.getElementById('PrograssBar');
 let gif=document.querySelector('.gif');
@@ -40,7 +42,43 @@ masterPlay.addEventListener('click',()=>{
 
     }
 })
+btn_1.addEventListener('click',()=>{
+    if(jahan.paused || jahan.currentTime===0){
+        jahan.play();
+        btn_1.classList.remove("fa-circle-play");
+        btn_1.classList.add("fa-circle-pause");
+        gif.classList.remove('opacity-0')
+        let inner=document.querySelector('.songInfo');
+        inner.textContent='Jahaan Tum Ho';
+        playFunction();
+    }
+    else{
+        jahan.pause();
+        btn_1.classList.remove("fa-circle-pause");
+        btn_1.classList.add("fa-circle-play");
+        gif.classList.add('opacity-0');
+        masterPlay.classList.remove("fa-circle-pause");
+        masterPlay.classList.add("fa-circle-play");
 
+    }
+})
+
+
+const playFunction=function(){
+        // jahan.play();
+        masterPlay.classList.remove("fa-circle-play");
+        masterPlay.classList.add("fa-circle-pause");
+        gif.classList.remove('opacity-0')
+        let inner=document.querySelector('.songInfo');
+        inner.textContent='Jahaan Tum Ho';
+    // else{
+    //     // jahan.pause();
+    //     masterPlay.classList.remove("fa-circle-pause");
+    //     masterPlay.classList.add("fa-circle-play");
+    //     gif.classList.add('opacity-0')
+
+    // }
+}
 // listin to events
 
 audioElement.addEventListener('timeupdate',()=>{
@@ -49,14 +87,20 @@ audioElement.addEventListener('timeupdate',()=>{
     let prograss=parseInt(audioElement.currentTime/audioElement.duration*100);
     myprograssBar.value=prograss;
 })
+jahan.addEventListener('timeupdate',()=>{
+    console.log('timeupdate');
+    // update seek bar
+    let prograss=parseInt(jahan.currentTime/jahan.duration*100);
+    myprograssBar.value=prograss;
+})
 
 myprograssBar.addEventListener('change',()=>{
     audioElement.currentTime=myprograssBar.value*audioElement.duration/100;
 })
 
-songItem.forEach((element,i)=>{
-    console.log(element,i);
-    // element.getElementByTagName("img")[0].src=songs[i].coverPath;
-    // element.getElementsByClassName('songName')[0].innerText=songs[i].songName;
+// songItem.forEach((element,i)=>{
+//     console.log(element,i);
+//     // element.getElementByTagName("img")[0].src=songs[i].coverPath;
+//     // element.getElementsByClassName('songName')[0].innerText=songs[i].songName;
 
-})
+// })
